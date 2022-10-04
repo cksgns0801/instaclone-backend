18ae5de86b41db6ae2e9864907c5bb52f086041c
L10H <cksgns080101@gmail.com>
2022-10-04 15:09:42 +0900

    1.3 backend setup

--- server.js
+++ server.js
@@ -1,14 +1,36 @@
 import { ApolloServer, gql } from "apollo-server";
 
 const typeDefs = gql`
-    type Query {
-        hello: String
+    type Movie{
+        title: String
+        year: Int
+
     }
+    type Query {
+        movies: [Movie]
+        movie: Movie
+    }
+    type Mutation {
+        createMovie(title: String!): Boolean
+        deleteMovie(title: String!): Boolean
+    }
+
 `;
 
 const resolvers = {
     Query: {
-        hello: () => "bebe",
+        movies: () => [],
+        movie: () => ({title: "hello", year: 2022})
+    },
+    Mutation: {
+        createMovie: (_, args) => {
+            console.log(args);
+            return true;
+        },
+        deleteMovie: (_, args) => {
+            console.log(args);
+            return true;
+        },
     },
 };
 
