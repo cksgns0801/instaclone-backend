b5f27ea2ead865dc5d9aa875852042a9178a029f
L10H <cksgns080101@gmail.com>
2022-10-06 16:18:07 +0900

    seeProfile

--- users/users.mutations.js
+++ users/users.mutations.js
@@ -3,29 +3,36 @@ import bcrypt from "bcrypt"
 
 export default {
     Mutation:{
-         createAccount: async (_,{ firstName,lastName,username,email,password }) => {
-            const existingUser = await client.user.findFirst({
-                where: {
-                    OR: [
-                        {
-                            username,
-                        },
-                        {
-                            email,
-                        }
-                    ]
+        createAccount: async (_,{ firstName,lastName,username,email,password }) => {
+            try {
+                const existingUser = await client.user.findFirst({
+                    where: {
+                        OR: [
+                            {
+                                username,
+                            },
+                            {
+                                email,
+                            }
+                        ]
+                    }
+                });
+                if(existingUser) {
+                    throw new Error ("This username/password is already taken.");
                 }
-            });
-            const uglyPassword = await bcrypt.hash(password, 10);
-            return client.user.create({
-                data: {
-                    username,
-                    email,
-                    firstName,
-                    lastName,
-                    password: uglyPassword,
-                },
-            });
+                const uglyPassword = await bcrypt.hash(password, 10);
+                return client.user.create({
+                    data: {
+                        username,
+                        email,
+                        firstName,
+                        lastName,
+                        password: uglyPassword,
+                    },
+                });
+            } catch (e) {
+                return e;
+            }
         },
     }
 };
