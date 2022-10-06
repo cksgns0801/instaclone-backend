bb9f7713dd594adb4144c248ba6fe4536fca1ea8
L10H <cksgns080101@gmail.com>
2022-10-06 16:53:59 +0900

    login

--- users/users.mutations.js
+++ users/users.mutations.js
@@ -34,5 +34,22 @@ export default {
                 return e;
             }
         },
+        login: async(_, { username, password }) => {
+            const user = await client.user.findFirst({where:{username}});
+            if (!user) {
+                return {
+                    ok: false,
+                    error: "User not found",
+                };
+            }
+            const passwordOk = await bcrypt.compare(password, user.password);
+            if (!passwordOk) {
+                return {
+                    ok: false,
+                    error: "Incorrect password",
+                };
+            }
+            console.log(passwordOk);
+        }
     }
 };
